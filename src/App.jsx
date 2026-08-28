9e30327fe87f0253deb4824dcb9266bd982e76cd
Ivan Posavi <ivan.posavi@gmail.com>
2026-08-28 19:32:43 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="app-placeholder">
+      <p className="app-placeholder__brand">ACME</p>
+      <h1>Expense Tracker</h1>
+      <p className="app-placeholder__subtitle">
+        Project setup is complete. The dashboard will be built in the next
+        phases.
+      </p>
+    </main>
+  )
+}
+
+export default App
